63f2b4716b0bfee75843287ab9916cde55a84b53
Liam's Laptop <liam.woodbaker@gmail.com>
2020-04-13 13:02:54 +1000

    Can't work out how to download the lecture recording

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ By Liam Wood-Baker, 2020
 """
 import time
 import os
+import urllib.request
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('prefs', {
@@ -67,6 +70,22 @@ def doDirectory(week):
         os.mkdir(directory)
     return directory
 
+
+def createTempDriver(options, directory):
+    """Creates a new driver variable which will download files to directory"""
+    # Once we have the links we want, and we need to download them to the appropriate directory. To do this, we have to
+    # make a new driver, with the default download directory changed (can't change the download directory without
+    # restarting the driver.
+    temp_options = options
+    temp_options.add_experimental_option('prefs', {
+        "download.default_directory": directory.replace('/', '\\'),  # apparently selenium broke forward slashes
+        "download.prompt_for_download": False,  # To auto download files
+        "download.directory_upgrade": True,
+        "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
+    })
+    return webdriver.Chrome('C:/Users/Liam/Drivers/chromedriver.exe', options=temp_options)
+
+
 def scrapeMAT1830():
     """Downloads the two tutorial sheets"""
     directory = doDirectory(week_number)
@@ -86,17 +105,7 @@ def scrapeMAT1830():
             solutions_link = tag.parent['href']
             print('found tutorial sheet solutions')
 
-    # Now we have the links we want, and we need to download them to the appropriate directory. To do this, we have to
-    # make a new driver, with the default download directory changed (can't change the download directory without
-    # restarting the driver.
-    temp_options = options
-    temp_options.add_experimental_option('prefs', {
-        "download.default_directory": directory.replace('/', '\\'),  # apparently selenium broke forward slashes
-        "download.prompt_for_download": False,  # To auto download files
-        "download.directory_upgrade": True,
-        "plugins.always_open_pdf_externally": True  # It will not show PDF directly in chrome
-    })
-    temp_driver = webdriver.Chrome('C:/Users/Liam/Drivers/chromedriver.exe', options=temp_options)
+    temp_driver = createTempDriver(options, directory)
     login(temp_driver)  # now we're logged in in the new window (which has the correct download directory)
 
     temp_driver.execute_script("window.open('');")
@@ -119,7 +128,31 @@ def scrapeATS2005():
             if 'Week ' + week_number in option.string:
                 driver.get(option['href'])
 
+    # finding the lecture
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    for resource in soup.findAll('span', class_='resourcelinkdetails'):
+        if 'Video file' in resource.string:
+            video_link = resource.parent.find('a', class_='')['href']
 
+    driver.get(video_link)  # get to the page that has the video embedded in it
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    final_link = soup.find('video').find('source')['src']  # the link is in the source tag, inside the video tag
+    driver.get(final_link)
+
+    # this doesn't work
+    # urllib.request.urlretrieve(final_link, 'videoname.mp4')
+
+    # this isn't needed if the above doesn't work
+    # # open a window with the correct download directory
+    # temp_driver = createTempDriver(options, directory)
+    # login(temp_driver)
+    #
+    # # open a new tab for the video
+    # temp_driver.execute_script("window.open('');")
+    # temp_driver.switch_to.window(temp_driver.window_handles[1])
+    #
+    # # get to the page which is actually the video
+    # temp_driver.get(final_link)
 
 # scrapeMAT1830()
 # driver.back()
